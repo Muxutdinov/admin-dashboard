b0b8023d6b9c3c07fee155741f2daaa18ffc6518
Jaxongir <jmuxutdinov@mail.ru>
2021-11-13 18:00:26 +0500

    topbar creating

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './App.css';
+import Topbor from './components/topbor/Topbor';
 
 
 const App = () => {
   return (
     <div>
-      
+      <Topbor/>
     </div>
   )
 }
